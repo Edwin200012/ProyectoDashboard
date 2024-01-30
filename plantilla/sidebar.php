3a01868a38d1bae5923580400ab81a80ef730a53
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-01-30 13:02:14 -0600

    Modificacion diseno sidebar

--- plantilla/sidebar.php
+++ plantilla/sidebar.php
@@ -1,3 +1,10 @@
+<style>
+#span1:hover {
+  color: yellow;
+}
+
+</style>
+
 <aside id="sidebar" class="sidebar" style="border-style: ridge; background-color: #3A1CA6; border-color: aliceblue;  border-width: 0.3px;">
 
     <ul class="sidebar-nav" id="sidebar-nav">
@@ -8,33 +15,34 @@
           <span style="color: white;">Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
-
+      <hr style="color:white; margin-top: 5px;">
       <li class="nav-item">
-        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link collapsed" data-bs-target="#components-nav"  href="acercadenosotros.php">
+        <a style="border-radius: 15px; background-color: #3A1CA6;  margin-top: -15px;" class="nav-link collapsed" data-bs-target="#components-nav"  href="acercadenosotros.php">
           <i style="color: white;" class="bi bi-menu-button-wide"></i><span style="color: white;">Acerca De Nosotros</span></i>
         </a>
       </li><!-- End Components Nav -->
-
+      <hr style="color:white; margin-top: 5px;">
       <li class="nav-item">
-        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link collapsed" data-bs-target="#forms-nav" href="servicios.php">
+        <a style="border-radius: 15px; background-color: #3A1CA6; margin-top: -15px;" class="nav-link collapsed" data-bs-target="#forms-nav" href="servicios.php">
           <i style="color: white;" class="bi bi-journal-text"></i><span style="color: white;">Servicios</span></i>
         </a>
         
       </li><!-- End Forms Nav -->
-
+      <hr style="color:white; margin-top: 5px;">
       <li class="nav-item">
-        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link collapsed" data-bs-target="#tables-nav" href="equipo.php">
+        <a style="border-radius: 15px; background-color: #3A1CA6;  margin-top: -15px;" class="nav-link collapsed" data-bs-target="#tables-nav" href="equipo.php">
           <i style="color: white;" class="bi bi-layout-text-window-reverse"></i><span style="color: white;">Equipo</span></i>
         </a>
         
       </li><!-- End Tables Nav -->
-
+      <hr style="color:white; margin-top: 5px;">
       <li class="nav-item">
-        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link collapsed" data-bs-target="#charts-nav" href="contacto.php">
+        <a style="border-radius: 15px; background-color: #3A1CA6;  margin-top: -15px;" class="nav-link collapsed" data-bs-target="#charts-nav" href="contacto.php">
           <i style="color: white;" class="bi bi-bar-chart"></i><span style="color: white;">Contacto</span></i>
         </a>
         
       </li><!-- End Charts Nav -->
+      <hr style="color:white; margin-top: 5px;">
     </ul>
 
   </aside><!-- End Sidebar-->
