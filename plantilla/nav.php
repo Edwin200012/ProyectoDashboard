3dfb1527b53a485e45f2620fb7d89f3323d6edfd
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-01-30 09:38:30 -0600

    Creacion de secciones y modificaciones

---
+++ plantilla/nav.php
@@ -0,0 +1,51 @@
+<header style="border-style: ridge; background-color: #3A1CA6; border-color: aliceblue;  border-width: 0.3px;" id="header" class="header fixed-top d-flex align-items-center">
+
+   <div class="d-flex align-items-center justify-content-between">
+      <a href="index.php" class="logo d-flex align-items-center">
+        <img src="assets/img/nuevo_logo.png" alt="">
+        <!-- <span class="d-none d-lg-block">NiceAdmin</span> -->
+      </a>
+      <i style="color: white;" class="bi bi-list toggle-sidebar-btn"></i>
+    </div>
+
+    <nav class="header-nav ms-auto">
+      
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <span style="color: white;" class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+          </a><!-- End Profile Iamge Icon -->
+
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <li class="dropdown-header">
+              <h6>Kevin Anderson</h6>
+              <span>Web Designer</span>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                <i class="bi bi-person"></i>
+                <span>Mi Perfil</span>
+              </a>
+            </li>
+            
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="#">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Cerrar Sesión</span>
+              </a>
+            </li>
+
+          </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
+
+      </ul>
+    </nav><!-- End Icons Navigation -->
+
+  </header><!-- End Header -->
---
+++ plantilla/sidebar.php
@@ -0,0 +1,40 @@
+<aside id="sidebar" class="sidebar" style="border-style: ridge; background-color: #3A1CA6; border-color: aliceblue;  border-width: 0.3px;">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link " href="index.php">
+          <i style="color: white;" class="bi bi-grid"></i>
+          <span style="color: white;">Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link collapsed" data-bs-target="#components-nav"  href="index.php">
+          <i style="color: white;" class="bi bi-menu-button-wide"></i><span style="color: white;">Acerca de Nosotros</span></i>
+        </a>
+      </li><!-- End Components Nav -->
+
+      <li class="nav-item">
+        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link collapsed" data-bs-target="#forms-nav" href="index.php">
+          <i style="color: white;" class="bi bi-journal-text"></i><span style="color: white;">Servicios</span></i>
+        </a>
+        
+      </li><!-- End Forms Nav -->
+
+      <li class="nav-item">
+        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link collapsed" data-bs-target="#tables-nav" href="index.php">
+          <i style="color: white;" class="bi bi-layout-text-window-reverse"></i><span style="color: white;">Equipo</span></i>
+        </a>
+        
+      </li><!-- End Tables Nav -->
+
+      <li class="nav-item">
+        <a style="border-radius: 15px; background-color: #3A1CA6;" class="nav-link collapsed" data-bs-target="#charts-nav" href="index.php">
+          <i style="color: white;" class="bi bi-bar-chart"></i><span style="color: white;">Contacto</span></i>
+        </a>
+        
+      </li><!-- End Charts Nav -->
+    </ul>
+
+  </aside><!-- End Sidebar-->
