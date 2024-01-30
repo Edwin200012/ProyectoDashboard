d93d33c05b98acb42fdc962efe758120f7590def
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-01-30 11:11:04 -0600

    Creacion del contenido de la seccion Acerca De Nosotros

--- plantilla/titulo.php
+++ plantilla/titulo.php
@@ -2,7 +2,7 @@
       <h1><?php echo $titulo; ?></h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
           <li class="breadcrumb-item active"><?php echo $titulo; ?></li>
         </ol>
       </nav>
