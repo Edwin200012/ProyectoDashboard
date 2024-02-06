5bc7d2625a1ddfd1d6d8c69eec6ff9fe2d870420
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-02-06 12:31:52 -0600

    Enviar parametros e insertar en base de datos

--- controllers/crearcuentausuario.php
+++ controllers/crearcuentausuario.php
@@ -1,21 +1,54 @@
 <?php
-    class UsuarioRegistro{
-        function Registro(){
+include_once("../route.php");
+    class Usuario{
+        function CrearUsuario(){
             $nombre = $_POST['nombre'];
             $correo = $_POST['correo'];
             $usuario = $_POST['usuario'];
             $contrasena = $_POST['contrasena'];
+
+            $url = Route::$url.Route::$crearUsuario;
+
+            $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, [
+                    'Content-Type: application/json',
+                ]);
+
+            $parametros = array (                    
+                    "nombre" => $nombre,
+                    "usuario" => $usuario,
+                    "correo" => $correo,
+                    "contrasena" => $contrasena
+            );
+
+            curl_setopt($curl, CURLOPT_POSTFIELDS,json_encode($parametros));
+
+            $respuesta = curl_exec($curl);
+            curl_close($curl);
+
+            $informacion = json_decode($respuesta);
+
+            if ($informacion->registrado)
+                {
+                    sleep(2);
+                    header('Location: ../login.php');
+                }
+                
+            else{
+                echo $respuesta;
+            }
             
-
-            var_dump($nombre);
-            var_dump($correo);
-            var_dump($usuario);
-            var_dump($contrasena);
-
         }
     }
 
-    $usuario = new UsuarioRegistro();
-    $usuario->Registro();
+    $usuario = new Usuario();
+    $usuario->CrearUsuario();
+
+    
+
 
 ?>
