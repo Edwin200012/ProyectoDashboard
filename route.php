95e6e8af3b11da2f80db1e01e2142deef12cb881
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-02-06 10:27:26 -0600

    Creacion de archivo route para crear un usuario

---
+++ route.php
@@ -0,0 +1,9 @@
+<?php
+class Route{
+    static $url = "https://localhost:7182/";
+    static $crearUsuario = "Usuario/CrearUsuario";//POST
+    // static $eliminarUsuario = "Usuario/Eliminar?idusuario=";
+    // static $mostrarporId = "Usuario/MostrarPorId?idusuario=";
+    // static $editarUsuario = "Usuario/Editar?idusuario=";
+}
+?>
