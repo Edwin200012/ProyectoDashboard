74c5dfc625c002533b301be6826c002067d1f8e4
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-02-12 11:35:05 -0600

    Editar Usuario Funciona

--- controllers/editarusuario.php
+++ controllers/editarusuario.php
@@ -26,7 +26,7 @@ include_once("../route.php");
 
             $curl = curl_init();
                 curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
                 curl_setopt($curl, CURLOPT_HTTPHEADER, [
@@ -34,7 +34,7 @@ include_once("../route.php");
                 ]);
 
             $parametros = array (           
-                    // "id" => 1,         
+                    "id" => $id,
                     "nombre" => $nombre,
                     "apellidop" => $apellidop,
                     "apellidom" => $apellidom,
