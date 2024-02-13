e058c9574b17ed1dba5bcf5c6de04d13f8bcb973
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-02-13 14:30:15 -0600

    Obtener id desde la sesion para modificar los datos del usuario

--- controllers/editarusuario.php
+++ controllers/editarusuario.php
@@ -1,8 +1,13 @@
 <?php
+session_start();
+
 include_once("../route.php");
     class Usuario{
         function EditarUsuario(){
-            // $id = $_POST['id'];
+            
+            $idsesion = $_SESSION['idsesion'];
+            $id = $idsesion;
+
             $nombre = $_POST['editarnombre'];
             $apellidop = $_POST['editarapellidop'];
             $apellidom = $_POST['editarapellidom'];
@@ -12,7 +17,7 @@ include_once("../route.php");
             $direccion = $_POST['editardireccion'];
             $contrasena = $_POST['editarconfirmarcontrasena'];
 
-            // var_dump($id);
+            var_dump($id);
             var_dump($nombre);
             var_dump($apellidop);
             var_dump($apellidom);
@@ -34,7 +39,7 @@ include_once("../route.php");
                 ]);
 
             $parametros = array (           
-                    // "id" => $id,
+                    "id" => $id,
                     "nombre" => $nombre,
                     "apellidop" => $apellidop,
                     "apellidom" => $apellidom,
