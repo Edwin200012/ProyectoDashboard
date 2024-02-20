d1548ba5829c449c177f5c8c456f486baee13e1a
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-02-20 12:49:00 -0600

    Borrar comentario

--- index.php
+++ index.php
@@ -6,13 +6,6 @@
     include_once ("plantilla/head.php");
   ?>
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Updated: Jan 09 2024 with Bootstrap v5.3.2
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
