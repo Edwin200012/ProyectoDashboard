52dbb88fbaa6cfabcb057672223943eefad98418
Diana Sánchez <sanchez.rdz.diana@gmail.com>
2024-02-22 10:53:45 -0600

    Creacion tabla y columnas para mostrar todos los registros de contacto en la seccion contacto

--- contacto.php
+++ contacto.php
@@ -76,140 +76,87 @@
   <div class="text-center">
     <button id="botonActualizar" style="width:300px; height:40px; border-radius: 30px; background-color: #77E6F2; color: #000807; border-color: silver;" class="btn btn-primary" type="submit">Enviar</button>
   </div>
-
 </form>
-
-
               </div>
             </div>
-      
-<!-- <div class="row"> -->
 
-        <!-- Left side columns -->
-        <!-- <div class="col-lg-8">
-          <div class="row"> -->
-
-            <!-- Sales Card -->
-            <!-- <div class="col-xxl-4 col-md-6">
+            <div style="margin-left: 50%; margin-top: -29%;" class="col-xxl-4 col-md-6">
               <div class="card info-card sales-card">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
                 <div class="card-body">
-                  <h5 class="card-title">Sales <span>| Today</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-cart"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>145</h6>
-                      <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
-                    </div>
-                  </div>
+                  <h5 class="card-title">Registro Contactos</span></h5>
                 </div>
-
-              </div> -->
-            <!-- </div> -->
-            <!-- End Sales Card -->
-
-            <!-- Revenue Card -->
-            <!-- <div class="col-xxl-4 col-md-6">
-              <div class="card info-card revenue-card">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body">
-                  <h5 class="card-title">Revenue <span>| This Month</span></h5>
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-currency-dollar"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>$3,264</h6>
-                      <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-                    </div>
-                  </div>
-                </div>
-              </div> -->
-            <!-- </div> -->
-            <!-- End Revenue Card -->
-
-            <!-- Customers Card -->
-            <!-- <div class="col-xxl-4 col-xl-12">
-
-              <div class="card info-card customers-card">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body">
-                  <h5 class="card-title">Customers <span>| This Year</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-people"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>1244</h6>
-                      <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
-                    </div>
-                  </div>
-
-                </div>
-              </div> -->
-
-            <!-- </div> -->
-            <!-- End Customers Card -->
-
-            
-
-          <!-- </div>
-        </div> -->
-        <!-- End Left side columns -->
-
-        <!-- Right side columns -->
-        <!-- <div class="col-lg-4"> -->
-
-
-        <!-- </div> -->
-        <!-- End Right side columns -->
-
-      <!-- </div> -->
+                 <!-- Table with stripped rows -->
+              <table class="table datatable">
+                <thead>
+                  <tr>
+                    <th>
+                      Id
+                    </th>
+                    <th>
+                      Correo
+                    </th>
+                    <th>
+                      Teléfono
+                    </th>
+                    <th>
+                      Ubicación
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Walker Nixon</td>
+                    <td>6901</td>
+                    <td>Metz</td>
+                    <td>41%</td>
+                  </tr>
+                  <tr>
+                    <td>Nathan Espinoza</td>
+                    <td>5956</td>
+                    <td>Strathcona County</td>
+                    <td>47%</td>
+                  </tr>
+                  <tr>
+                    <td>Kelly Cameron</td>
+                    <td>4836</td>
+                    <td>Fontaine-Valmont</td>
+                    <td>24%</td>
+                  </tr>
+                  <tr>
+                    <td>Kyra Moses</td>
+                    <td>3796</td>
+                    <td>Quenast</td>
+                    <td>68%</td>
+                  </tr>
+                  <tr>
+                    <td>Grace Bishop</td>
+                    <td>8340</td>
+                    <td>Rodez</td>
+                    <td>4%</td>
+                  </tr>
+                  <tr>
+                    <td>Haviva Hernandez</td>
+                    <td>8136</td>
+                    <td>Suwałki</td>
+                    <td>16%</td>
+                  </tr>
+                  <tr>
+                    <td>Alisa Horn</td>
+                    <td>9853</td>
+                    <td>Ucluelet</td>
+                    <td>39%</td>
+                  </tr>
+                  <tr>
+                    <td>Zelenia Roman</td>
+                    <td>7516</td>
+                    <td>Redwater</td>
+                    <td>31%</td>
+                  </tr>
+                </tbody>
+              </table>
+              <!-- End Table with stripped rows -->
+              </div>
+            </div>
     </section>
 
   </main><!-- End #main -->
