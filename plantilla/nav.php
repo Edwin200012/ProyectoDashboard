163b097aabda8731f695caad2c981cbfe32e3020
Edwin200012 <edwin_vazquez200012@hotmail.com>
2024-03-21 12:53:59 -0600

    Mostrar swal alert al cerrar sesion

--- plantilla/nav.php
+++ plantilla/nav.php
@@ -1,3 +1,8 @@
+<style>
+  .dropdown-item:hover {
+  cursor: pointer;
+}
+</style>
 <?php 
     session_start();
     if(isset($_SESSION['idsesion']) == false){
@@ -15,7 +20,6 @@
       exit;
     }
 
-
     //Actualizar tiempo de la última actividad
     $_SESSION['ultima_actividad'] = time();
   
@@ -31,7 +35,19 @@
     $rutasesion = $_SESSION['rutasesion'];
 ?>
 
+<!-- Include SweetAlert library -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 <header style="border-style: ridge; background-color: #3A1CA6; border-color: aliceblue;  border-width: 0.3px;" id="header" class="header fixed-top d-flex align-items-center">
+
+<!-- Agrega el spinner y el overlay dentro de un div que esté oculto inicialmente -->
+<div id="spinner-cerrar-sesion" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 9999; display: flex; justify-content: center; align-items: center;">
+  <div class="spinner-border text-light" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>
 
    <div class="d-flex align-items-center justify-content-between">
       <a href="index.php" class="logo d-flex align-items-center">
@@ -67,12 +83,14 @@
               <hr class="dropdown-divider">
             </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="./sesion/cerrarsesion.php">
+            <li class="li-item">
+              <a class="dropdown-item d-flex align-items-center">
+              <!-- <a class="dropdown-item d-flex align-items-center"> -->
                 <i class="bi bi-box-arrow-right"></i>
-                <span>Cerrar Sesión</span>
+                <span id="btnCerrarSesion">Cerrar Sesión</span>
               </a>
             </li>
+            <!-- <button id="btnCerrarSesion" class="btn btn-primary">Mostrar Alerta</button> -->
 
           </ul><!-- End Profile Dropdown Items -->
         </li><!-- End Profile Nav -->
@@ -81,3 +99,45 @@
     </nav><!-- End Icons Navigation -->
 
   </header><!-- End Header -->
+
+  
+<script>
+  $(document).ready(function() {
+    // Agrega un controlador de eventos clic al botón
+    $('#btnCerrarSesion').click(function() {
+      // Muestra el SweetAlert
+      Swal.fire({
+      background: '#f3f4f6',
+      title: '¿Desea cerrar sesión?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#28a745',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Si, cerrar sesión',
+      cancelButtonText: 'Cancelar',
+      customClass: {
+    confirmButton: 'btn-rounded', // Clase para redondear el botón de confirmar
+    cancelButton: 'btn-rounded' // Clase para redondear el botón de cancelar
+  }
+      }).then((result) => {
+      if (result.isConfirmed) {
+         document.getElementById('spinner-cerrar-sesion').style.display = 'flex';
+        setTimeout(function() {
+          window.location.href = "./sesion/cerrarsesion.php";
+        }, 1000);
+
+        
+}
+      
+    });
+  });
+});
+
+// Ocultar el spinner y el overlay cuando la página haya cargado completamente
+window.addEventListener('load', function() {
+    document.getElementById('spinner-cerrar-sesion').style.display = 'none';
+  });
+
+</script>
+
+
