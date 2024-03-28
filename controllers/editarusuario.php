c7e0c930962bd9d4ffc0d796bc38b41363e498d2
Edwin200012 <edwin_vazquez200012@hotmail.com>
2024-03-28 10:53:18 -0600

    Crear funcion para enviar notificacion al correo del usuario al actualizar sus datos

--- controllers/editarusuario.php
+++ controllers/editarusuario.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
-
 include_once("../route.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
     class Usuario{
         function EditarUsuario(){
             
@@ -72,7 +81,7 @@ include_once("../route.php");
                     $_SESSION['telefonosesion'] = $telefono;
                     $_SESSION['direccionsesion'] = $direccion;
                     // $_SESSION['rutasesion'] = $ubicacionFinal;
-                    
+                    $this->NotificacionDatosActualizados();
                     // echo $_SESSION;
                     header('Location: ../miperfil.php?actualizadatos=true');
                 }
@@ -82,6 +91,46 @@ include_once("../route.php");
             }
             
         }
+
+
+//Funcion para enviar notificacion al cambiar contrasena
+function NotificacionDatosActualizados(){
+
+    $mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = $_ENV['HOST'];                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = $_ENV['USERNAME'];                     //SMTP username
+    $mail->Password   = $_ENV['PASSWORD'];                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = $_ENV['PORT'];                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Destinatarios
+    $mail->setFrom('edwinvazquezcal12@gmail.com', 'Remitente');
+
+    //Añadir un destinatario
+    // $mail->addAddress('fawigek423@glaslack.com');
+    $mail->addAddress((isset($_SESSION['correosesion']) ? $_SESSION['correosesion'] : 'Desconocido'));
+    //Contenido
+    $mail->isHTML(true);
+    $mail->Subject = 'Actualizacion de datos del Usuario: ' . (isset($_SESSION['usuariosesion']) ? $_SESSION['usuariosesion'] : 'Desconocido');
+    $mail->Body    = 'Este es un correo electronico para confirmar que tus datos han sido actualizados exitosamente. Si no hiciste estos cambios, por favor contacta a soporte.';
+    $mail->AltBody = 'Este es un correo electonico para confirmar que tus datos han sido actualizados exitosamente. Si no hiciste estos cambios, por favor contacta a soporte.';
+
+    $mail->send();
+    echo 'El mensaje ha sido enviado';
+
+    } catch (Exception $e) {
+        echo "El mensaje no pudo ser enviado. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+
     }
 
     $usuario = new Usuario();
