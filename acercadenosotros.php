f8b50adcebbd8189c8dfd904b258313a50db7b2b
Edwin200012 <edwin_vazquez200012@hotmail.com>
2024-05-16 10:32:13 -0600

    Cambios

--- acercadenosotros.php
+++ acercadenosotros.php
@@ -70,36 +70,6 @@
     <button id="botonEnviarAcercaDeNosotros" style="width:300px; height:40px; border-radius: 30px; background-color: #77E6F2; color: #000807; border-color: silver;" class="btn btn-primary" type="submit">Enviar</button>
   </div>
 </form>
-
-                  <!-- <h5 class="card-title">Acerca de Nosotros</h5>
-                  <div style="width: 100%;" class="row content">
-                    <div>
-                    <p class="centered-text" style="margin-bottom: 2%;">
-                      En Arsha, estamos comprometidos a ofrecer soluciones de software innovadoras y de alta calidad que impulsen el éxito de nuestros clientes. 
-                      Desde nuestra fundación en 2010, hemos estado liderando el camino en el desarrollo de software, proporcionando servicios personalizados que satisfacen las necesidades únicas de cada cliente.<br>
-                      Arsha fue fundada por un equipo de apasionados por la tecnología que compartían una visión común: simplificar la vida de las personas a través de soluciones digitales inteligentes. 
-                      Lo que comenzó como un pequeño proyecto en un garaje se ha convertido en una empresa reconocida a nivel nacional por su excelencia en el desarrollo de software.
-                    </p>
-                    </div>
-                    <div class="col-lg-6">
-                      <h5>Misión</h5>
-                      <p>
-                      Nuestra misión en Arsha es aprovechar la tecnología para transformar la forma en que las empresas operan y se relacionan con sus clientes. 
-                      Estamos dedicados a proporcionar soluciones de software que sean intuitivas, confiables y que impulsen el crecimiento empresarial.
-                      En Arsha, estamos comprometidos a ofrecer soluciones de software innovadoras y de alta calidad que impulsen el éxito de nuestros clientes.
-                      Desde nuestra fundación en 2010, hemos estado liderando el camino en el desarrollo de software, proporcionando servicios personalizados que satisfacen las necesidades únicas de cada cliente.
-                     </p>
-                    </div>
-                  <div class="col-lg-6 pt-4 pt-lg-0">
-                     <h5>Visión</h5>
-                      <p>
-                      En Arsha, nuestra visión es liderar la vanguardia de la innovación tecnológica, ofreciendo soluciones de software de vanguardia que transformen industrias y mejoren la vida de las personas en todo el mundo.
-                      Nos esforzamos por ser reconocidos como el socio preferido para empresas que buscan impulsar su éxito a través de la tecnología, destacando por nuestra dedicación a la excelencia, la innovación constante y la colaboración con nuestros clientes para superar desafíos y alcanzar metas ambiciosas.
-                    </p>
-                </div>
-                  </div> -->
-
-
                 </div>
               </div>
             </div>
